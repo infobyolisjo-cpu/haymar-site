5271d868670b99ccd680fc8943cf71e234cc819f
Olis Acosta <infobyolisjo@gmail.com>
2025-09-01 10:37:48 -0400

    Fix: tailwind.config.js limpio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",       // incluye todo lo que está en /app
-    "./components/**/*.{js,ts,jsx,tsx}" // incluye componentes
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        brand: "#0b5fff",   // azul corporativo
-        accent: "#25D366",  // verde WhatsApp
-      },
-      fontFamily: {
-        sans: ["Inter", "ui-sans-serif", "system-ui"], // tipografía moderna
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
