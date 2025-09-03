5ea89dfceb148c807e3fed49b0e90a719d167c6f
Olis Acosta <infobyolisjo@gmail.com>
2025-09-03 10:22:26 -0400

    fix: tsconfig válido, next.config.js y página mínima de 4 enlaces

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {};
+module.exports = nextConfig;
